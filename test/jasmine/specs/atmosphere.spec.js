1a81a5cf401a8bc7db81fda84b036e073835bbcd
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2017-02-25 17:43:19 -0600

    Finished getStrata method

--- test/jasmine/specs/atmosphere.spec.js
+++ test/jasmine/specs/atmosphere.spec.js
@@ -3,10 +3,27 @@ define(function(require, exports, module) {
 
     var atmlib = require('index').atmosphere;
 
+    var TROPOSPHERE_VALUES = [
+        0,
+        3.7,
+        11
+    ];
+    var THERMOSPHERE_VALUES = [
+        87,
+        95.6,
+        450.1,
+        500
+    ];
+
     describe('Atmosphere module', function() {
         it('can determine layer of atmosphere from altitude', function() {
             var getStrata = atmlib.getStrata;
-            console.log(getStrata(11));
+            TROPOSPHERE_VALUES.forEach(function(altitude) {
+                expect(getStrata(altitude)).toEqual('troposphere');
+            });
+            THERMOSPHERE_VALUES.forEach(function(altitude) {
+                expect(getStrata(altitude)).toEqual('thermosphere');
+            });
         });
         xit('can convert FPS <<>> mach', function() {
             //code
