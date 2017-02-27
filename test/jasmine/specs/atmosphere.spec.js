cac363d0bc04def74ce62d2e15ff1277ac6c3d3b
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2017-02-27 17:35:45 -0600

    Added tests for atmosphere module

--- test/jasmine/specs/atmosphere.spec.js
+++ test/jasmine/specs/atmosphere.spec.js
@@ -32,8 +32,22 @@ define(function(require, exports, module) {
             expect(mol(21)).toEqual(16.17);
             expect(mol(22)).toEqual(28.964);
         });
-        xit('can convert FPS <<>> mach', function() {
-            //code
+        it('can calculate speed of sound based on altitude', function() {
+            var speed = atmlib.calculate.speedOfSound;
+            expect(speed()).toBeCloseTo(340.9, 1)// speed of sound at sea-level, 340.29
+            expect(speed(1000)).toBeCloseTo(295.6, 1); // 336.103
+            expect(speed(10000)).toBeCloseTo(295.6, 1);// 299.63,  299.5 (1962, 1976)
+            expect(speed(50000)).toBeCloseTo(332.8, 1);// 329.799, 329.8 (1962, 1976)
+            expect(speed(80000)).toBeCloseTo(267.1, 1);// 269.44,  282.5 (1962, 1976)
+            expect(speed(84000)).toBeCloseTo(272.1, 1);// 269.44,  276.9 (1962, 1976)
+            expect(speed(86000)).toBeCloseTo(274.6, 1);// 269.44,  274.1 (1962, 1976)
+        });
+        it('can convert m/s <<>> mach', function() {
+            var speed = atmlib.calculate.speedOfSound;
+            var toMach = atmlib.convert.metersPerSecondToMach;
+            expect(toMach(speed())).toEqual(1);
+            expect(toMach(speed(), 0)).toEqual(1);
+            expect(toMach(speed(10000), 10000)).toEqual(1);
         });
     });
 });
