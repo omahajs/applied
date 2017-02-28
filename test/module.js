cd7610169987544521f299275e490129a1fe5c47
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2017-02-28 16:05:37 -0600

    Added some dev deps and prettified module test:build

--- test/module.js
+++ test/module.js
@@ -1,3 +1,18 @@
+var chalk = require('chalk');
 var applied = require('../applied');
-console.log(Object.keys(applied));
-console.log(Object.keys(applied.geodetic));
+var atm = applied.atmosphere;
+var geo = applied.geodetic;
+
+var longitude = 32.7157;
+var speed = atm.calculate.speedOfSound();
+
+console.log('');
+console.log(chalk.bold('Speed of Sound: ') + chalk.cyan(speed));
+console.log('');
+console.log(chalk.bold('Longitude: '));
+console.log(chalk.dim('  Decimal: ') + chalk.cyan(longitude));
+console.log(chalk.dim('  hybrid:  ') + chalk.cyan(geo.convert.toDegreesDecimalMinutes(longitude)));
+console.log(chalk.dim('  DMS:     ') + chalk.cyan(geo.convert.toDegreesMinutesSeconds(longitude)));
+console.log('');
+console.log(chalk.bold.green('✓ ') + chalk.bold('Everything looks OK'));
+console.log('');
